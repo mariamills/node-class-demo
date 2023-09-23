e8908f9ee9000a001ced1a9997492223cb5c0201
Maria Mills <cxwinterx@gmail.com>
2023-09-23 13:52:52 -0400

    Add dotenv, update styles, and display API data

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const port = 3000;
+
+const axios = require('axios');
+
+const API_KEY = process.env.NASA_API_KEY;
 
 // Set view template engine
 app.set('view engine', 'ejs');
 
 app.use('/static', express.static('public'));
 
-app.get('/', (req, res) => res.render('home.ejs'));
+app.get('/', (req, res) => {
+  axios
+    .get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+    .then((response) => {
+      res.render('home.ejs', { data: response.data });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send('There was an error fetching the data.');
+    });
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
