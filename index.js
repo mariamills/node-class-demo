78ea161bcd980f16723ea401ed7aaa4a4211a77e
Maria Mills <cxwinterx@gmail.com>
2023-09-23 12:23:57 -0400

    Install EJS, add Bootstrap & CSS stylesheet

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// Set view template engine
+app.set('view engine', 'ejs');
+
+app.use('/static', express.static('public'));
+
+app.get('/', (req, res) => res.render('home.ejs'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
